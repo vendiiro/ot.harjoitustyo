7b97cddb497c279deefc14729c7ea5b5e68f6cb4
vendiiro <iiro.vendelin@gmail.com>
2020-03-19 11:11:21 +0200

    testit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -15,6 +15,40 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
     }
+
+    @Test
+    public void saldoOikein() {
+        assertEquals("Kortilla on rahaa 10.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void lataaminenOikein() {
+        kortti.lataaRahaa(25);
+        assertEquals("Kortilla on rahaa 35.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void vaheneminenOikein() {
+        kortti.otaRahaa(5);
+        assertEquals("Kortilla on rahaa 5.0 euroa", kortti.toString());
+
+    }
+
+    @Test
+    public void josRahaaEiTarpeeksi() {
+        kortti.otaRahaa(10);
+        kortti.otaRahaa(3);
+        assertEquals("Kortilla on rahaa 0.0 euroa", kortti.toString());
+    }
+    @Test 
+    public void riittaaRahat(){
+        assertTrue(kortti.otaRahaa(2));
+    }
+    @Test
+    public void eiRiitaRahat(){
+        assertFalse(kortti.otaRahaa(20));
+    }
+
 }
