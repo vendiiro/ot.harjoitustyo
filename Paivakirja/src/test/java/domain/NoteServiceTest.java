09fb4d35f2d383dbc882c3fb0ee400208ed2c3b7
vendiiro <iiro.vendelin@gmail.com>
2020-04-14 12:05:57 +0300

    testi

--- Paivakirja/src/test/java/domain/NoteServiceTest.java
+++ Paivakirja/src/test/java/domain/NoteServiceTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domain;
 
 import java.io.IOException;
@@ -86,7 +81,7 @@ public class NoteServiceTest {
     }
 
     @Test
-    public void checkingIfUserIsLoggedInWorksWhenUserIsNotLoggedIn() {
+    public void checkingIsUserLoggedInWorksWhenUserNotLoggedIn() {
         assertFalse(ns.isUserLoggedIn());
     }
 
